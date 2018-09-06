110627156207a03a2b912fadc964a579beb265a3
Darien Chong <E0176647@u.nus.edu>
2018-09-06 23:35:10 +0800

    Add functionality for sort command Add tests for sort command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,21 +1,37 @@
 package seedu.addressbook.commands;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.ReadOnlyPerson;
 
 public class SortCommand extends Command {
 	public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the Address Book by a given sort key. "
-            + "Parameters: [index] [name] [email] [address]\n"
+            + "Parameters: [name] [email] [address]\n"
             + "Example: " + COMMAND_WORD
             + " name";
     
+    public static final String ILLEGAL_SORTKEY_MESSAGE = "Unsupported sort key indicated!";
+    
     private static final String[] KEYWORDS = new String[] {
-    		"index",
     		"name",
     		"email",
     		"address"
     };
+    
+    /** Note the order of the sortKey method names should correspond to the index of the keyword in KEYWORDS */
+    private static final String[] SORTKEY_METHOD_NAMES = new String[] {
+    		"getName",
+    		"getEmail",
+    		"getAddress"
+    };	
     
     // We create the Pattern to match our keywords (only one of the keywords)
     // based on the keywords we indicate.
@@ -36,14 +52,41 @@ public class SortCommand extends Command {
     // Matches exactly one of the keywords
     public static final Pattern KEYWORDS_ARGS_FORMAT = Pattern.compile(KEYWORDS_ARGS_FORMAT_BUILDER.toString());
     
-    private final String sortKey;
+    private final String keyExtractorName;
     
     public SortCommand(String key) {
-    	sortKey = key;
+    	keyExtractorName = getKeyExtractorName(key);
+    }
+    
+    public String getKeyExtractorName(String key) {
+    	try {
+    		return SORTKEY_METHOD_NAMES[Arrays.asList(KEYWORDS).indexOf(key)];
+    	} catch (ArrayIndexOutOfBoundsException e) {
+    		return "";
+    	}
     }
     
     @Override
     public CommandResult execute() {
-    	return new CommandResult("Sort command has not been implemented yet.");
+    	try {
+	    	Method keyExtractor = Person.class.getMethod(keyExtractorName);
+	    	
+	    	addressBook.sort(new Comparator<Person>() {
+	    		@Override
+	    		public int compare(Person p1, Person p2) {
+	    			try {
+						return (keyExtractor.invoke(p1).toString()).compareTo(keyExtractor.invoke(p2).toString());
+					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+						e.printStackTrace();
+						return 0; // arbitrary choice
+					}
+	    		}
+	    	});
+    	} catch (NoSuchMethodException e) {
+    		return new CommandResult(ILLEGAL_SORTKEY_MESSAGE);
+    	}
+    	
+    	List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }
--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -1,5 +1,79 @@
 package seedu.addressbook.commands;
 
-public class SortCommandTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.util.TypicalPersons;
+
+public class SortCommandTest {
+	private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
+	private final TypicalPersons td = new TypicalPersons();
+	
+	@Test
+	public void execute() {
+		assertSortCommandInvalidKey("fgdfgs");
+		
+		assertSortCommandBehavior("name", getResultAfterSorting(new Comparator<Person>() {
+			@Override
+			public int compare(Person p1, Person p2) {
+				return p1.getName().toString().compareTo(p2.getName().toString());
+			}
+		}));
+		
+		assertSortCommandBehavior("email", getResultAfterSorting(new Comparator<Person>() {
+			@Override
+			public int compare(Person p1, Person p2) {
+				return p1.getEmail().toString().compareTo(p2.getEmail().toString());
+			}
+		}));
+		
+		assertSortCommandBehavior("address", getResultAfterSorting(new Comparator<Person>() {
+			@Override
+			public int compare(Person p1, Person p2) {
+				return p1.getAddress().toString().compareTo(p2.getAddress().toString());
+			}
+		}));
+	}
+	
+	private void assertSortCommandBehavior(String keyword, List<ReadOnlyPerson> expectedPersonList) {
+        SortCommand command = createSortCommand(keyword);
+        CommandResult result = command.execute();
+
+        assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.getFeedbackToUser());
+    }
+	
+	private void assertSortCommandInvalidKey(String keyword) {
+		SortCommand command = createSortCommand(keyword);
+		CommandResult result = command.execute();
+		
+		assertEquals(SortCommand.ILLEGAL_SORTKEY_MESSAGE, result.getFeedbackToUser());
+	}
+	
+	private SortCommand createSortCommand(String keyword) {
+        SortCommand command = new SortCommand(keyword);
+        command.setData(addressBook, Collections.emptyList());
+        return command;
+    }
+	
+	private List<ReadOnlyPerson> getResultAfterSorting(Comparator<? extends Person> sorter) {
+		addressBook.sort(new Comparator<Person>() {
+			@Override
+			public int compare(Person p1, Person p2) {
+				return p1.getName().toString().compareTo(p2.getName().toString());
+			}
+		});
+		List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+		return allPersons;
+	}
 }
